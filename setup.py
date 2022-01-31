5036099662e8a3e152c7284930fda77f14696352
Maxime Arthaud <maxime@arthaud.me>
2022-01-30 19:48:13 -0800

    Bump the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='git-dumper',
-    version='1.0.2',
+    version='1.0.3',
     py_modules=['git_dumper'],
     entry_points={
         'console_scripts': [
